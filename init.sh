b2a63743f94c24d39c771d4bc71c93d72bb21e7d
Tsai-Ming Tseng <tsai-ming.tseng@oracle.com>
2014-09-09 05:14:21 -0700

    add bash_aliases

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 DIR=$(dirname $0)
-
+echo $DIR
 ln -s $DIR/eclimrc ~/.eclimrc
 ln -s $DIR/gvimrc ~/.gvimrc
 ln -s $DIR/tmux.conf ~/.tmux.conf
 ln -s $DIR/vim ~/.vim
 ln -s $DIR/vimrc ~/.vimrc
-
+ln -s $DIR/bash_aliases ~/.bash_aliases
